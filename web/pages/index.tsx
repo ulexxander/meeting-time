6c5f0c4016fe5b33ebe6767982952eb804b6e453
Alexander Ustyugov <ulexxander@gmail.com>
2022-04-23 16:44:33 +0200

    web - added Apollo GraphQL client

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,14 +1,51 @@
+import { gql } from "@apollo/client";
 import { NextPage } from "next";
 import Head from "next/head";
+import { client } from "../graphql/client";
 
-const Home: NextPage = () => {
+const query = gql`
+  query TeamByID($id: ID!) {
+    teamByID(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query,
+    variables: {
+      id: "1",
+    },
+  });
+
+  return {
+    props: {
+      team: data.teamByID,
+    },
+  };
+}
+
+type Team = {
+  id: number;
+  name: string;
+};
+
+const Home: NextPage<{ team: Team }> = ({ team }) => {
   return (
     <div>
       <Head>
         <title>Home page</title>
       </Head>
 
-      <p style={{ textAlign: "center" }}>Hello Next!</p>
+      <div style={{ textAlign: "center" }}>
+        <h1>Hello Next!</h1>
+
+        <p>
+          Team: {team.name} #{team.id}
+        </p>
+      </div>
     </div>
   );
 };
